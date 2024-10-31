40de28f652426d945d39ad641611021a6a0b272a
Ole Norstrand <ole.norstrand@bekk.no>
2024-10-31 16:38:49 +0100

    a start

---
+++ app/app/routes/articles.$slug.tsx
@@ -0,0 +1,43 @@
+import type {MetaFunction} from '@remix-run/node'
+import {Post} from "../../utils/sanity/types/sanity.types";
+import {POST_BY_SLUG} from "../../utils/sanity/queries/postQueries";
+import {useLoaderData} from "@remix-run/react";
+import {loadQuery} from "../../utils/sanity/store";
+import {PortableText} from "@portabletext/react";
+
+export const meta: MetaFunction = () => {
+  return [{title: 'New Remix App'}, {name: 'description', content: 'Welcome to Remix!'}]
+}
+
+
+export async function loader({params}: { params: { slug: string } }) {
+  const {data: post} = await loadQuery<Post>(POST_BY_SLUG, {slug: params.slug})
+  return post
+}
+
+const components = {
+  types: {
+    code: () => (
+      <div>
+        <p>HELL YES</p>
+      </div>
+    ),
+    imageWithMetadata: () => (
+      <div>
+        <p>FUCKEN PICHURE</p>
+      </div>
+    )
+  }
+}
+
+export default function Index() {
+  const post = useLoaderData<Post>()
+  console.log(post)
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      {post?.content && <PortableText value={post.content} components={components} />}
+    </>
+  )
+}
---
+++ app/utils/sanity/portable-text/CodeBlock.tsx
@@ -0,0 +1,64 @@
+import {PrismLight as SyntaxHighlighter} from "react-syntax-highlighter";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import clojure from "react-syntax-highlighter/dist/cjs/languages/prism/clojure";
+import csharp from "react-syntax-highlighter/dist/cjs/languages/prism/csharp";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import diff from "react-syntax-highlighter/dist/cjs/languages/prism/diff";
+import elm from "react-syntax-highlighter/dist/cjs/languages/prism/elm";
+import fsharp from "react-syntax-highlighter/dist/cjs/languages/prism/fsharp";
+import haskell from "react-syntax-highlighter/dist/cjs/languages/prism/haskell";
+import java from "react-syntax-highlighter/dist/cjs/languages/prism/java";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import lisp from "react-syntax-highlighter/dist/cjs/languages/prism/lisp";
+import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
+import kotlin from "react-syntax-highlighter/dist/cjs/languages/prism/kotlin";
+import less from "react-syntax-highlighter/dist/cjs/languages/prism/less";
+import html from "react-syntax-highlighter/dist/cjs/languages/prism/markup";
+import nix from "react-syntax-highlighter/dist/cjs/languages/prism/nix";
+import python from "react-syntax-highlighter/dist/cjs/languages/prism/python";
+import rust from "react-syntax-highlighter/dist/cjs/languages/prism/rust";
+import scala from "react-syntax-highlighter/dist/cjs/languages/prism/scala";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import swift from "react-syntax-highlighter/dist/cjs/languages/prism/swift";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import prism from "react-syntax-highlighter/dist/cjs/styles/prism/prism";
+import {Code} from "../types/sanity.types";
+
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("clojure", clojure);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("diff", diff);
+SyntaxHighlighter.registerLanguage("elm", elm);
+SyntaxHighlighter.registerLanguage("fsharp", fsharp);
+SyntaxHighlighter.registerLanguage("csharp", csharp);
+SyntaxHighlighter.registerLanguage("haskell", haskell);
+SyntaxHighlighter.registerLanguage("html", html);
+SyntaxHighlighter.registerLanguage("java", java);
+SyntaxHighlighter.registerLanguage("javascript", jsx);
+SyntaxHighlighter.registerLanguage("js", jsx);
+SyntaxHighlighter.registerLanguage("json", json);
+SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("kotlin", kotlin);
+SyntaxHighlighter.registerLanguage("less", less);
+SyntaxHighlighter.registerLanguage("lisp", lisp);
+SyntaxHighlighter.registerLanguage("nix", nix);
+SyntaxHighlighter.registerLanguage("python", python);
+SyntaxHighlighter.registerLanguage("rust", rust);
+SyntaxHighlighter.registerLanguage("scala", scala);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("sh", bash);
+SyntaxHighlighter.registerLanguage("swift", swift);
+SyntaxHighlighter.registerLanguage("ts", tsx);
+SyntaxHighlighter.registerLanguage("typescript", tsx);
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+
+export const CodeBlock = (code: Code) => {
+  if (!code?.code) {
+    return null;
+  }
+  return (
+      <SyntaxHighlighter style={prism} language={code.language ?? "text"}>
+        {code.code}
+      </SyntaxHighlighter>
+  );
+};
