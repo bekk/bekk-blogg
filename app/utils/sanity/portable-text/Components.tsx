386721e0681d07277a123ff69bb40a110c001590
Ole Norstrand <ole.norstrand@bekk.no>
2024-11-04 08:50:27 +0100

    klar for oppdeling av oppgaver

--- app/utils/sanity/portable-text/Components.tsx
+++ app/utils/sanity/portable-text/Components.tsx
@@ -4,11 +4,35 @@ import ImageWithMetadataBlock from "./ImageWithMetadataBlock";
 
 export const components = {
   types: {
-    code: (props: {value: Code}) => (
-      <CodeBlock code={props.value} />
+    code: (props: { value: Code }) => (
+      <CodeBlock code={props.value}/>
     ),
-    imageWithMetadata: (props: {value: ImageWithMetadata}) => (
-      <ImageWithMetadataBlock image={props.value} />
-    )
+    imageWithMetadata: (props: { value: ImageWithMetadata }) => (
+      <ImageWithMetadataBlock image={props.value}/>
+    ),
+    codeSandbox: <p>FILL IN</p>,
+    codePen: <p>FILL IN</p>,
+    youtube: <p>FILL IN</p>,
+    twitter: <p>FILL IN</p>,
+    unfurledUrl: <p>FILL IN</p>,
+    mainImage: <p>FILL IN</p>,
+    iframe: <p>FILL IN</p>,
+    image: <p>FILL IN</p>,
+    infoBlock: <p>FILL IN</p>,
+    __block: <p>FILL IN</p>,
   }
+  // marks: {
+  //   link: (props: any) => (
+  //     <TextLink href={props.mark.href}>{props.children}</TextLink>
+  //   ),
+  //   code: Code,
+  // },
+  // list:
+  //   withWrap()(
+  //   (props: { type: "number" | "bullet"; children: React.ReactNode }) => {
+  //     const ListComponent =
+  //       props.type === "number" ? OrderedList : UnorderedList;
+  //     return <ListComponent fontSize="xl">{props.children}</ListComponent>;
+  //   },
+  // ),
 }
