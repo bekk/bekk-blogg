f97afa117c90d236a6cb49c97218c40d6b22ef50
Anna Li <halloannali@gmail.com>
2024-11-05 14:13:25 +0100

    rewrote youtube

--- app/app/routes/articles.$slug.tsx
+++ app/app/routes/articles.$slug.tsx
@@ -6,13 +6,11 @@ import { components } from '../../utils/sanity/portable-text/Components'
 import { POST_BY_SLUG } from '../../utils/sanity/queries/postQueries'
 import { loadQuery } from '../../utils/sanity/store'
 import { Post } from '../../utils/sanity/types/sanity.types'
-
 import styles from '~/styles/articles.css?url'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
-
 
 export async function loader({ params }: { params: { slug: string } }) {
   const { data: post } = await loadQuery<Post>(POST_BY_SLUG, { slug: params.slug })
@@ -27,9 +25,9 @@ export default function Index() {
   const post = useLoaderData<Post>()
 
   return (
-      <div className="article-container">
-        <h1>{post.title}</h1>
-        {post?.content && <PortableText value={post.content} components={components} />}
+    <div className="article-container">
+      <h1>{post.title}</h1>
+      {post?.content && <PortableText value={post.content} components={components} />}
     </div>
   )
 }
--- app/utils/sanity/portable-text/YouTubeBlock.tsx
+++ app/utils/sanity/portable-text/YouTubeBlock.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import getYouTubeId from 'get-youtube-id'
 import YouTube from 'react-youtube'
+import { Youtube as YouTubeSanityType } from '../types/sanity.types'
+// import useMediaQuery from '~/hooks/useMediaQuery'
 
-export const YouTubeBlock = ({ node }: any) => {
-  if (!node) {
+type YouTubeProps = {
+  youtube: YouTubeSanityType
+}
+
+export const YouTubeBlock = ({ youtube }: YouTubeProps) => {
+  // const isSm = useMediaQuery('(min-width: 640px)')
+  // const height = isSm ? '500px' : '300px'
+
+  if (!youtube || !youtube.url) {
     return null
   }
-  const id = getYouTubeId(node.url || node.src)
+
+  const id = getYouTubeId(youtube.url)
+
   return (
-    <div className="h-[32rem]">
-      <YouTube videoId={id as string} opts={{ width: '100%', height: '300px' }} />
+    <div>
+      <YouTube videoId={id as string} opts={{ width: '100%', height: '100%' }} />
     </div>
   )
 }
