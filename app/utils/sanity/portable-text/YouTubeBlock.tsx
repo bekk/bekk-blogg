b7ee65d1381a21f754695ef3ef4ee7ecb39d0ed6
Anna Li <halloannali@gmail.com>
2024-11-05 12:30:19 +0100

    rewrote youtube

--- app/utils/sanity/portable-text/YouTubeBlock.tsx
+++ app/utils/sanity/portable-text/YouTubeBlock.tsx
@@ -1,37 +1,15 @@
-import { Youtube } from '../types/sanity.types'
+import React from 'react'
+import getYouTubeId from 'get-youtube-id'
+import YouTube from 'react-youtube'
 
-type YouTubeBlockProps = {
-  youtube: Youtube
-}
-
-export const YouTubeBlock = ({ youtube }: YouTubeBlockProps) => {
-  if (!youtube || !youtube.url) {
+export const YouTubeBlock = ({ node }: any) => {
+  if (!node) {
     return null
   }
-
-  const url = new URL(youtube.url)
-
-  if (url.hostname !== 'youTube.com') {
-    console.error(`The URL provided was not a YouTube URL. Instead, it was "${url}"`)
-    return null
-  }
-
-  let [, id] = url.pathname.split('/')
-
-  if (!id) {
-    console.error('Could not find the YouTube id from the URL', url)
-    return null
-  }
-
+  const id = getYouTubeId(node.url || node.src)
   return (
-    <iframe
-      style={{ width: '100%' }}
-      height="auto"
-      src={`https://www.youtube.com/embed/${id}`}
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
+    <div className="h-[32rem]">
+      <YouTube videoId={id as string} opts={{ width: '100%', height: '300px' }} />
+    </div>
   )
 }
