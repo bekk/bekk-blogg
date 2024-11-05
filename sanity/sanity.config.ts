c952d180f3b65fb75885ad8fe398f7bcdf8cf33f
Ole Norstrand <ole.norstrand@bekk.no>
2024-11-05 12:31:10 +0100

    sanity preview done

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -25,6 +25,33 @@ const config = defineConfig({
   projectId: 'ah2n1vfr',
   dataset: 'bekk-blogg',
 
+  document: {
+    // prev is the result from previous plugins and thus can be composed
+    productionUrl: async (prev, context) => {
+      // context includes the client and other details
+      const {getClient, dataset, document} = context
+      const client = getClient({apiVersion: process.env.SANITY_STUDIO_API_VERSION ?? '2021-03-25'})
+
+      if (document._type === 'post') {
+        const slug = await client.fetch(
+          `*[_type == 'post' && _id == $postId][0].slug.current`,
+          {postId: document._id}
+        )
+
+        console.log("JEEPIS")
+        console.log(document._id)
+
+        const params = new URLSearchParams()
+        params.set('preview', 'true')
+        params.set('dataset', dataset)
+
+        return `${process.env.SANITY_STUDIO_FRONTEND_URL}/articles/${slug}?${params}`
+      }
+
+      return prev
+    },
+  },
+
   plugins: [
     structureTool(),
     visionTool(),
