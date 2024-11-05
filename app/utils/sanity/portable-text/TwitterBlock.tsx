c035741b55f4c62fcd98fb3c9a7bce2ddfb521be
Ole Norstrand <ole.norstrand@bekk.no>
2024-11-05 09:56:52 +0100

    twitter fungerer nå

---
+++ app/utils/sanity/portable-text/TwitterBlock.tsx
@@ -0,0 +1,26 @@
+import {Twitter} from "../types/sanity.types";
+import {TwitterTweetEmbed} from "react-twitter-embed";
+
+const getTweetId = (url?: string) => {
+  if (!url) {
+    return null;
+  }
+  const parsedUrl = new URL(url);
+  return parsedUrl.pathname.split("/").pop();
+};
+
+type TwitterBlockProps = {
+  twitter: Twitter;
+};
+
+export default function TwitterBlock({ twitter }: TwitterBlockProps) {
+  const id = getTweetId(twitter?.url ?? undefined);
+  if (!id) {
+    return null;
+  }
+  return (
+    <>
+      <TwitterTweetEmbed tweetId={id} />
+    </>
+  );
+}
