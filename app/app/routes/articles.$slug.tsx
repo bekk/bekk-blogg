afeae73ccbd846bbdc6316f5a2e6819b0a129699
majatestad <maja_ying@hotmail.com>
2024-11-05 14:16:59 +0100

    added frame

--- app/app/routes/articles.$slug.tsx
+++ app/app/routes/articles.$slug.tsx
@@ -1,35 +1,35 @@
-import {PortableText} from "@portabletext/react";
-import type {MetaFunction} from '@remix-run/node'
-import {useLoaderData} from "@remix-run/react";
+import { PortableText } from '@portabletext/react'
+import type { MetaFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
-import {components} from "../../utils/sanity/portable-text/Components";
-import {POST_BY_SLUG} from "../../utils/sanity/queries/postQueries";
-import {loadQuery} from "../../utils/sanity/store";
-import {Post} from "../../utils/sanity/types/sanity.types";
+import { components } from '../../utils/sanity/portable-text/Components'
+import { POST_BY_SLUG } from '../../utils/sanity/queries/postQueries'
+import { loadQuery } from '../../utils/sanity/store'
+import { Post } from '../../utils/sanity/types/sanity.types'
 
 import styles from '~/styles/articles.css?url'
 
 export const meta: MetaFunction = () => {
-  return [{title: 'New Remix App'}, {name: 'description', content: 'Welcome to Remix!'}]
+  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
 }
 
 
-export async function loader({params}: { params: { slug: string } }) {
-  const {data: post} = await loadQuery<Post>(POST_BY_SLUG, {slug: params.slug})
+export async function loader({ params }: { params: { slug: string } }) {
+  const { data: post } = await loadQuery<Post>(POST_BY_SLUG, { slug: params.slug })
   return post
 }
 
 export function links() {
-  return [{rel: 'stylesheet', href: styles}]
+  return [{ rel: 'stylesheet', href: styles }]
 }
 
 export default function Index() {
   const post = useLoaderData<Post>()
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      {post?.content && <PortableText value={post.content} components={components}/>}
-    </>
+      <div className="article-container">
+        <h1>{post.title}</h1>
+        {post?.content && <PortableText value={post.content} components={components} />}
+    </div>
   )
 }
