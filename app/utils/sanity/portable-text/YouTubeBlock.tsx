fe84be1d1afdbaa30972728d8403c48226ccf38a
Anna Li <anna.li@bekk.no>
2024-11-06 10:18:52 +0100

    Portable text youtube komponent (#24)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Board-15686f16cc3d4c22905c805070b1d501?p=1346bd3085418013b633c69fb6c39171&pm=s)
    
    🐛 Type oppgave: Brukerhistorie
    
    ## Løsning
    
    #️⃣ Punktliste av hva som er endret:
    
    - Lagt til serializer for youtube i portable-text
    - Lagt til useMediaQuery-hook
    
    ## 🧪 Testing
    
    Gå til /articles/clickrelief-click-without-clicking og test at youtube
    videoen funker.
    Opprett gjerne en ny artikkel, legg til youtube-lenke og se om den
    dukker opp riktig i artikkelen.
    
    ## Bilder
    ![Screenshot 2024-11-05 at 15 13
    15](https://github.com/user-attachments/assets/659303f4-0ba0-4b87-bbce-94399f7f8e44)

---
+++ app/utils/sanity/portable-text/YouTubeBlock.tsx
@@ -0,0 +1,32 @@
+import { Youtube } from '../types/sanity.types'
+import useMediaQuery from '~/hooks/useMediaQuery'
+import getYouTubeId from 'get-youtube-id'
+
+type YouTubeProps = {
+  youtube: Youtube
+}
+
+export const YouTubeBlock = ({ youtube }: YouTubeProps) => {
+  const isSm = useMediaQuery('(min-width: 640px)')
+  const height = isSm ? '500px' : '300px'
+
+  if (!youtube || !youtube.url) {
+    return null
+  }
+
+  const id = getYouTubeId(youtube.url)
+
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height={height}
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  )
+}
