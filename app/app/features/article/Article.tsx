5eb549d4d25723b9a780c1b02dfd67dae52c40c0
Anna Li <halloannali@gmail.com>
2024-11-07 11:41:09 +0100

    styling changes

--- app/app/features/article/Article.tsx
+++ app/app/features/article/Article.tsx
@@ -8,11 +8,11 @@ type ArticleProps = {
 
 export const Article = ({ post }: ArticleProps) => {
   return (
-    <div className="md:grid sm:grid-cols-[auto_auto] md:grid-rows-[auto_auto] md: gap-12">
+    <div className="md:grid sm:grid-cols-[auto_auto] md:grid-rows-[auto_auto] md:gap-20 p-6 md:p-20">
       <div className="col-start-2 col-end-2 row-start-1 row-end-1 md:text-right">poststamp</div>
-      <div className="col-start-1 col-span-2 row-start-1 row-end-1">breadcrumbs</div>
-      <div className="col-start-1 col-end-1 row-start-2 row-end-2 meta">
-        <h1>{post.title}</h1>
+      <div className="col-start-1 col-span-2 row-start-1 row-end-1 hidden">breadcrumbs</div>
+      <div className="col-start-1 col-end-1 row-start-2 row-end-2 meta mb-8">
+        <h1 className="font-delicious mb-4">{post.title}</h1>
       </div>
       <div className="col-start-2 col-end-2 row-start-2 row-end-2">
         {post?.description && (
