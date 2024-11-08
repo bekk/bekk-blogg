06884eb69e0fcbf017f2b42b67086a852eb68f92
majatestad <maja_ying@hotmail.com>
2024-11-08 08:52:50 +0100

    set default text size for body

--- web/app/features/article/Article.tsx
+++ web/app/features/article/Article.tsx
@@ -20,16 +20,15 @@ export const Article = ({ post }: ArticleProps) => {
       <div className="col-start-1 col-end-1 row-start-2 row-end-2 meta mb-8">
         <h1 className="font-delicious mb-8">{post.title}</h1>
         {post.tags && (
-          <div className="text-body-mobile md:text-body-desktop border-b border-bekk-night pb-1 mb-8">
+          <>
             {post.tags && post.tags.map((tag) => tag.name).join(', ')}
-          </div>
+            <HLine />
+          </>
         )}
-        <div className="text-body-mobile md:text-body-desktop border-b border-bekk-night pb-1 mb-8">
-          {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
-        </div>
-        <div className="text-body-mobile md:text-body-desktop border-b border-bekk-night pb-1">
-          {post.availableFrom && formatDate(post.availableFrom)}
-        </div>
+        {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
+        <HLine />
+        {post.availableFrom && formatDate(post.availableFrom)}
+        <HLine />
       </div>
       <div className="col-start-2 col-end-2 row-start-2 row-end-2">
         {post?.description && (
@@ -38,12 +37,13 @@ export const Article = ({ post }: ArticleProps) => {
           </div>
         )}
         {post?.content && (
-          <div className="text-body-mobile md:text-body-desktop">
-            <PortableText value={post.content} components={components} />
-          </div>
+          <PortableText value={post.content} components={components} />
         )}
       </div>
     </div>
   )
 }
 
+export const HLine = () => {
+  return <div className="border-b border-bekk-night pb-1 mb-8"></div>
+}
