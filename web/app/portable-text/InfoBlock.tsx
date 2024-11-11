a488e2f6c8df407b7c41a17f7dea72c9579eadf0
Kristian Rosland <kristian.rosland@bekk.no>
2024-11-11 15:16:44 +0100

    Styling av lenker og inline code 💄  (#52)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/1c622bcecc4643e8add71399d1bd3cf7?v=be83ed3fb16e4bddbd0720601a0a0d16&p=1346bd308541802db8a3e9aa0a76a723&pm=s)
    
    🐛 Type oppgave: Feature/styling
    
    🥅 Mål med PRen: Lesere må se at en lenke er klikkbar, og inline kode
    skal skille seg ut av resten av teksten
    
    ## Løsning
    
    🆕 Endring: Legge til korrekt styling på lenker og inline code
    
    #️⃣ Punktliste av hva som er endret:
    
    - Lagt til `marks` i Components, slik at den plukker opp lenker og
    inline-code riktig
    - Laget en TextLink-komponent som rendrer en link (ekstern eller intern)
    - Litt styling av inline code
    - Endret enda litt mer i føkkings eslint og prettier. Hvis det ikke
    funker nå så går hele repoet på dynga
    - Obs: Endra også sånn at en tom linje rendres som en <br/> for at man
    skal kunne legge til litt mer luft i artikkelen selv.
    
    
    ## Bilder
    
    _Ved frontendendringer legg ved bilde av før og etter._
    
    **Før:**
    
    ![image](https://github.com/user-attachments/assets/6f27bb39-3f60-455a-be01-f5dc4df0a5fa)
    
    
    **Etter:**
    
    ![image](https://github.com/user-attachments/assets/8b329c72-7896-4dd1-98e0-21ffc0fc08ed)
    
    ![image](https://github.com/user-attachments/assets/a4e70e45-9a0f-4b23-a883-283bf6df0509)

--- web/app/portable-text/InfoBlock.tsx
+++ web/app/portable-text/InfoBlock.tsx
@@ -1,9 +1,9 @@
 import { PortableText } from '@portabletext/react'
-import { PortableText as SanityPortableTextType } from 'utils/sanity/types/sanity.types'
+import { PortableText as PortableTextType } from 'utils/sanity/types/sanity.types'
 
 import { components } from './Components'
 
-export const InfoBlock = ({ content }: { content: SanityPortableTextType }) => {
+export const InfoBlock = ({ content }: { content: PortableTextType }) => {
   return (
     <div className="rounded bg-envelope-beige px-4 pb-1 pt-4">
       <PortableText value={content} components={components} />
