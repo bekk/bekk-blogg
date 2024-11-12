38ba56e4d85b95ab9621af40fb45bce3133133b5
Vemund Thallaug Lund <vemund@thallaug.org>
2024-11-12 13:56:31 +0100

    dynamically adjust based on screen size

--- web/app/features/door/Letter.tsx
+++ web/app/features/door/Letter.tsx
@@ -1,8 +1,7 @@
 import { Post } from '../../../utils/sanity/types/sanity.types'
 import { PortableText } from '@portabletext/react'
 import { PostStamp } from '~/features/article/PostStamp'
-import readingTime from "reading-time";
-
+import readingTime from 'reading-time'
 
 type LetterProps = {
   post: Post
@@ -10,25 +9,25 @@ type LetterProps = {
 export const Letter = ({ post }: LetterProps) => {
   return (
     <div className="striped-frame w-10/12 p-3 md:p-8">
-      <div className="sm:grid-cols-[1000fr_1fr_1000fr] md:grid md:grid-rows-[auto_auto] max-w-6xl">
-        <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 mb-8 p-5 mr-5">
-          <h2 className="font-delicious mb-20">{post.title}</h2>
+      <div className="max-w-6xl grid sm:grid-cols-[auto] md:grid md:grid-rows-[auto]">
+        <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-5 sm:p-5">
+          <div className='max-sm:mb-8 sm:mb-20 max-sm:w-10/12'>
+            <h2 className="font-delicious">{post.title}</h2>
+          </div>
           {post.tags && post.tags.map((tag) => tag.name).join(', ')}
-          <div className="mb-8 border-b border-bekk-night pb-1" />
-          {readingTime(post.content?.toString() || "").text.replace("read", "")}
-          <div className="mb-8 border-b border-bekk-night pb-1" />
-          {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
-          <div className="mb-8 border-b border-bekk-night pb-1" />
+          <div className="max-sm:mb-3 mb-6 border-b border-bekk-night pb-1" />
+          {readingTime(post.content?.toString() || '').text.replace('read', '')}
+          <div className="max-sm:mb-3 mb-6 border-b border-bekk-night pb-1" />
+          {post.authors && `Fra: ${post.authors.map((author) => author.fullName).join(', ')}`}
+          <div className="max-sm:mb-3 mb-6 border-b border-bekk-night pb-1" />
         </div>
-        <div className="col-start-2 row-start-1 row-end-13 border-r-2 border-b border-bekk-night hidden md:block" />
+        <div className="col-start-2 row-start-1 row-end-13 hidden border-b border-r-2 border-bekk-night sm:block" />
 
-        <div className='col-start-3 col-end-3 row-start-2 row-end-3 ml-5'>
-          <div className='mb-8 flex justify-end'>
+        <div className="col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-5">
+          <div className="sm:mb-8 max-sm:ml-3 flex justify-end">
             <PostStamp />
           </div>
-          <div className='hidden md:block'>
-            {post.description && <PortableText value={post.description} />}
-          </div>
+          <div className="hidden sm:block">{post.description && <PortableText value={post.description} />}</div>
         </div>
       </div>
     </div>
