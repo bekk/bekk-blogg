f65ec9d721741ff2572948295c98ce087bb984c9
Vemund Thallaug Lund <80754916+vemundtl@users.noreply.github.com>
2024-11-12 16:32:10 +0100

    Styling/lukeside (#57)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Styling-brev-p-lukesiden-1386bd3085418091b20dc33d2307244b?pvs=4)
    
    💄 Styling
    
    🥅 Mål med PRen: Fikse lukesiden
    
    ## Løsning
    
    🆕 Endring: Styling på lukeside + read time 
    
    #️⃣ Punktliste av hva som er endret:
    
    - Endra Letter-komponenten til å i alle fall ligne på figma-skissen 
    - Er også lagt opp til å dynamisk endre seg avhengig av skjermstørrelse
    - Lagt til read time på både artikkel- og luke-siden 
    - Conditional rendering på kategori på artikkelsiden
    
    ## 🧪 Testing
    
    Om man synes at størrelsen på kortene blir for rare for de forskjellige
    skjermstørrelsene eller om det ser greit ut?
    
    ## Bilder
    
    **Før:**
    
    ![image](https://github.com/user-attachments/assets/eaf22c72-0234-4809-8dc4-ead35f7ddea5)
    
    
    
    **Etter:**
    
    
    ![image](https://github.com/user-attachments/assets/33c73889-6519-4e23-a432-97e82a383aa5)
    
    
    ![image](https://github.com/user-attachments/assets/02ad5a8d-09b3-4a9a-8c84-392079f07aec)

--- web/app/features/article/Article.tsx
+++ web/app/features/article/Article.tsx
@@ -5,6 +5,7 @@ import { Post } from 'utils/sanity/types/sanity.types'
 import { PostStamp } from '~/features/article/PostStamp'
 import { components } from '~/portable-text/Components'
 import ImageBlock from '~/portable-text/ImageBlock'
+import readingTime from 'reading-time'
 
 type ArticleProps = {
   post: Post
@@ -19,9 +20,16 @@ export const Article = ({ post }: ArticleProps) => {
       <div className="col-span-2 col-start-1 row-start-1 row-end-1 hidden">breadcrumbs</div>
       <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 mb-8">
         <h1 className="mb-8 font-delicious">{post.title}</h1>
-        <div className="mb-8 border-b border-bekk-night pb-1 text-body-mobile md:text-body-desktop">
-          {post.tags && post.tags.map((tag) => tag.name).join(', ')}
-        </div>
+        {post.tags && (
+          <div className="mb-8 border-b border-bekk-night pb-1 text-body-mobile md:text-body-desktop">
+            {post.tags.map((tag) => tag.name).join(', ')}
+          </div>
+        )}
+        {post.content && (
+          <div className="mb-8 border-b border-bekk-night pb-1 text-body-mobile md:text-body-desktop">
+            {readingTime(post.content.toString() || '').text.replace('read', '')}
+          </div>
+        )}
         <div className="mb-8 border-b border-bekk-night pb-1 text-body-mobile md:text-body-desktop">
           {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
         </div>
--- web/app/features/door/Letter.tsx
+++ web/app/features/door/Letter.tsx
@@ -1,17 +1,39 @@
 import { Post } from '../../../utils/sanity/types/sanity.types'
+import { PortableText } from '@portabletext/react'
+import { PostStamp } from '~/features/article/PostStamp'
+import readingTime from 'reading-time'
 
 type LetterProps = {
   post: Post
 }
 export const Letter = ({ post }: LetterProps) => {
   return (
-    <div className="striped-frame p-3 md:p-8">
-      <h2 className="font-delicious">{post.title}</h2>
-      <div className="mb-8 border-b border-bekk-night pb-1" />
-      {post.tags && post.tags.map((tag) => tag.name).join(', ')}
-      <div className="mb-8 border-b border-bekk-night pb-1" />
-      {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
-      <div className="mb-8 border-b border-bekk-night pb-1" />
+    <div className="striped-frame min-w-full max-sm:p-3 sm:p-7">
+      <div className="grid max-w-4xl grid-cols-[1fr_1px_1fr] max-sm:grid-cols-[30fr_1fr]">
+        <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
+          <div className="max-sm:mb-7 sm:mb-20">
+            <h2 className="font-delicious">{post.title}</h2>
+          </div>
+          {post.tags && (
+            <>
+              {post.tags.map((tag) => tag.name).join(', ')}
+              <div className="mb-7 border-b border-bekk-night pb-1 max-sm:mb-3" />
+            </>
+          )}
+          {readingTime(post.content?.toString() || '').text.replace('read', '')}
+          <div className="mb-7 border-b border-bekk-night pb-1 max-sm:mb-3" />
+          {post.authors && `Fra: ${post.authors.map((author) => author.fullName).join(', ')}`}
+          <div className="mb-7 border-b border-bekk-night pb-1 max-sm:mb-3" />
+        </div>
+        <div className="col-start-2 row-start-1 row-end-3 hidden border-r border-bekk-night sm:block" />
+
+        <div className="col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
+          <div className="flex justify-end max-sm:ml-1 sm:mb-9">
+            <PostStamp />
+          </div>
+          <div className="hidden sm:block">{post.description && <PortableText value={post.description} />}</div>
+        </div>
+      </div>
     </div>
   )
 }
