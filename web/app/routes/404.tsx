188fda8e64943da7ad122bf21435603b60b8ba98
Maja Testad <62019920+majatestad@users.noreply.github.com>
2024-11-14 15:49:06 +0100

    added 404 page (#76)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/404-side-12e6bd3085418009870dccc33849413e?pvs=4)
    
    🐛 Type oppgave: brukerhistorie
    
    
    ## Løsning
    
    #️⃣ Punktliste av hva som er endret:
    
    - laget 404 side
    
    ## 🧪 Testing
    
    
    ## Bilder
    
    **Etter:**
    
    ![image](https://github.com/user-attachments/assets/d266e272-c285-4600-af26-e25b5a9c645f)

---
+++ web/app/routes/404.tsx
@@ -0,0 +1,11 @@
+export const Page404 = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-10 pt-14 gap-6 md:p-40 md:gap-8">
+      <h1>Oi, det ser ut som du har gått deg vill i julekalenderen!</h1>
+      <h2>
+        Denne siden finnes ikke, men fortvil ei - det er mange andre luker å åpne. Kanskje nissen kan hjelpe deg
+        tilbake? 🎅
+      </h2>
+    </div>
+  )
+}
