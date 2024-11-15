575d3bcbbd2fc96cb5c79de4a49021a4eb044d9d
Vemund Thallaug Lund <vemund@thallaug.org>
2024-11-15 13:37:10 +0100

    fix alignment within an article

--- web/app/features/navigation/Header.tsx
+++ web/app/features/navigation/Header.tsx
@@ -1,7 +1,8 @@
 import { Link } from '@remix-run/react'
 
+import { PostStamp } from '../article/PostStamp'
+
 import { BekkLogo } from '~/features/article/BekkLogo'
-import { PostStamp } from '~/features/article/PostStamp'
 import { useBreadcrumbs } from '~/hooks/useBreadcrumbs'
 
 type HeaderProps = {
@@ -13,7 +14,7 @@ export const Header = ({ isInArticle = false }: HeaderProps) => {
 
   return (
     <div
-      className={`flex items-end justify-between pb-8 md:pb-14 ${isInArticle ? 'pt-6 md:px-6 md:pt-5' : 'pt-4 md:px-10 md:pt-8'}`}
+      className={`flex items-end justify-between pb-8 md:pb-14 ${isInArticle ? 'pt-6 px-2 md:px-16 md:pt-5' : 'pt-4 md:px-10 md:pt-8'}`}
     >
       <div className="min-w-0 flex-1">
         <ol className="scrollbar-none flex overflow-x-auto px-4">
