505f248075373bb339b630b36580ee9dcd718088
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-19 15:43:18 +0100

    Første forsøk på visual editing

---
+++ web/utils/sanity/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@sanity/client'
+
+import { sanityConfig } from './config'
+
+export const client = createClient({
+  ...sanityConfig,
+  useCdn: true,
+  perspective: 'published',
+})
