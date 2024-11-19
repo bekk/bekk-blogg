0aa307f416a05feeb892d6ee36b4373c25d8e9f1
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-19 13:36:16 +0100

    Fiks bug hvor spotify embeds fikk en intern scroll

--- web/app/portable-text/PodcastBlock.tsx
+++ web/app/portable-text/PodcastBlock.tsx
@@ -1,5 +1,13 @@
 type PodcastProps = { src: string; title: string }
 
 export default function PodcastBlock({ podcast }: { podcast: PodcastProps }) {
-  return <iframe src={podcast.src} title={podcast.title} width="100%" />
+  return (
+    <iframe
+      src={podcast.src}
+      title={podcast.title}
+      className="w-full overflow-hidden"
+      // Note: scrolling="no" er deprekert, men det finnes ingen annen måte å få riktig høyde på iframe'en på visstnok
+      scrolling="no"
+    />
+  )
 }
