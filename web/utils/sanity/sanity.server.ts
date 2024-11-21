b480025c91aead287fc22826f3c7fb2719d1f407
majatestad <maja_ying@hotmail.com>
2024-11-21 12:17:27 +0100

    use 'avif' if possible, 'webp' if not

--- web/utils/sanity/sanity.server.ts
+++ web/utils/sanity/sanity.server.ts
@@ -23,4 +23,4 @@ export const readClient = createClient({
 })
 
 const builder = imageUrlBuilder(readClient)
-export const urlFor = (source: SanityImageSource) => builder.image(source).format('webp').quality(80).width(850)
+export const urlFor = (source: SanityImageSource) => builder.image(source).auto('format').quality(80).width(850)
