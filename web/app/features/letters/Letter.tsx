b974c5a88057449530c94129d3b542de47c75e25
Natasha Tikhonova <64562118+natashatikhonova@users.noreply.github.com>
2024-11-21 14:25:10 +0100

    Styling/ingress (#109)
    ## Beskrivelse
    
    💳 Lenke til [Notionkort
    1](https://www.notion.so/bekks/For-lang-ingress-p-brevet-1446bd30854180fdb7e1d9bf48667c0d?pvs=4)
    💳 Lenke til [Notionkort
    2](https://www.notion.so/bekks/Aligne-toppen-av-ingressen-og-toppen-av-meta-1446bd3085418062b8e0de0aab047b22?pvs=4)
    
    🐛 Type oppgave: _Styling_
    
    🥅 Mål med PRen: Aligner ingress med meta og setter maks lengde på
    ingress
    
    ## Løsning
    
    #️⃣ Punktliste av hva som er endret:
    
    - satt makslengde på ingress på brevet til å være 250 characters
    - endrer på grid på brevet for å aligne ingress med meta
    
    ## 🧪 Testing
    
    Sjekk at brevene med ulik innhold ser bra ut på mobil og desktop
    
    ## Bilder
    
    **Før:**
    <img width="1028" alt="image"
    src="https://github.com/user-attachments/assets/7589e6fe-3c0c-495e-934e-2cf93f35b459">
    
    **Etter:**
    <img width="1010" alt="image"
    src="https://github.com/user-attachments/assets/dd6269ef-af4e-4ea2-93e5-a1b901344975">

--- web/app/features/letters/Letter.tsx
+++ web/app/features/letters/Letter.tsx
@@ -2,8 +2,7 @@ import { readingTime } from 'utils/readTime'
 import { toPlainText } from 'utils/sanity/utils'
 
 import { Post } from '../../../utils/sanity/types/sanity.types'
-
-import { PostStamp } from '~/features/article/PostStamp'
+import { PostStamp } from '../article/PostStamp'
 
 type LetterProps = {
   post: Post
@@ -13,32 +12,32 @@ type LetterProps = {
 export const Letter = ({ post, showReadTime = true }: LetterProps) => {
   return (
     <div className="striped-frame min-w-full py-6 px-6 sm:p-7">
-      <div className="grid max-w-4xl sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr]">
-        <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
+      <div className="grid  max-w-4xl sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr]">
+        <div className="col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
           <h2 className="font-delicious sm:mb-20">{post.title}</h2>
-          <div className="text-sm sm:text-lg">
-            {post.tags && (
-              <>
-                {post.tags.map((tag) => tag.name).join(', ')}
-                <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-              </>
-            )}
-            {post.type === 'podcast' && post.podcastLength
-              ? `${post.podcastLength} min`
-              : showReadTime
-                ? readingTime(post.content)
-                : ''}
-            {showReadTime ? <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" /> : null}
-            {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
-            <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-          </div>
         </div>
-        <div className="col-start-2 row-start-1 row-end-3 hidden border-r border-bekk-night sm:block" />
-        <div className="col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
-          <div className="flex justify-end ml-1 sm:mb-9">
-            <PostStamp size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />
-          </div>
-          <div className="hidden sm:text-lg sm:block">{post.description && toPlainText(post.description)}</div>
+        <div className="col-start-2 row-start-1 row-end-4 hidden border-r border-bekk-night sm:block" />
+        <div className="flex justify-end ml-1 sm:mb-9 col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
+          <PostStamp size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />{' '}
+        </div>
+        <div className="text-sm sm:text-lg col-start-1 col-end-1 row-start-3 row-end-3 sm:mr-7">
+          {post.tags && (
+            <>
+              {post.tags.map((tag) => tag.name).join(', ')}
+              <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+            </>
+          )}
+          {post.type === 'podcast' && post.podcastLength
+            ? `${post.podcastLength} min`
+            : showReadTime
+              ? readingTime(post.content)
+              : ''}
+          {showReadTime ? <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" /> : null}
+          {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
+          <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+        </div>
+        <div className="hidden sm:text-lg sm:block col-start-3 col-end-3 row-start-3 row-end-3 sm:ml-7">
+          <p className="line-clamp-6"> {post.description && toPlainText(post.description)} </p>
         </div>
       </div>
     </div>
