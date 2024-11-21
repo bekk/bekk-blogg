9783a43003a6e3d3e535e31f8f0471603bf16815
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2024-11-21 12:15:07 +0100

    use truncate and update grid in letter

--- web/app/features/letters/Letter.tsx
+++ web/app/features/letters/Letter.tsx
@@ -1,9 +1,9 @@
 import { readingTime } from 'utils/readTime'
 import { toPlainText } from 'utils/sanity/utils'
+import { truncateText } from 'utils/truncateText'
 
 import { Post } from '../../../utils/sanity/types/sanity.types'
-
-import { PostStamp } from '~/features/article/PostStamp'
+import { PostStamp } from '../article/PostStamp'
 
 type LetterProps = {
   post: Post
@@ -11,34 +11,35 @@ type LetterProps = {
 }
 
 export const Letter = ({ post, showReadTime = true }: LetterProps) => {
+  const MAX_DESCRIPTION_LENGTH = 250
   return (
     <div className="striped-frame min-w-full py-6 px-6 sm:p-7">
-      <div className="grid max-w-4xl sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr]">
-        <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
+      <div className="grid  max-w-4xl sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr]">
+        <div className="col-start-1 col-end-1 row-start-2 row-end-2 ">
           <h2 className="font-delicious sm:mb-20">{post.title}</h2>
-          <div className="text-sm sm:text-lg">
-            {post.tags && (
-              <>
-                {post.tags.map((tag) => tag.name).join(', ')}
-                <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-              </>
-            )}
-            {post.type === 'podcast' && post.podcastLength
-              ? `${post.podcastLength} min`
-              : showReadTime
-                ? readingTime(post.content)
-                : ''}
-            {showReadTime ? <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" /> : null}
-            {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
-            <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-          </div>
         </div>
-        <div className="col-start-2 row-start-1 row-end-3 hidden border-r border-bekk-night sm:block" />
-        <div className="col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
-          <div className="flex justify-end ml-1 sm:mb-9">
-            <PostStamp size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />
-          </div>
-          <div className="hidden sm:text-lg sm:block">{post.description && toPlainText(post.description)}</div>
+        <div className="col-start-2 row-start-1 row-end-4 hidden border-r border-bekk-night sm:block" />
+        <div className="flex justify-end ml-1 sm:mb-9 col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
+          <PostStamp size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />{' '}
+        </div>
+        <div className="text-sm sm:text-lg col-start-1 col-end-1 row-start-3 row-end-3 sm:mr-7">
+          {post.tags && (
+            <>
+              {post.tags.map((tag) => tag.name).join(', ')}
+              <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+            </>
+          )}
+          {post.type === 'podcast' && post.podcastLength
+            ? `${post.podcastLength} min`
+            : showReadTime
+              ? readingTime(post.content)
+              : ''}
+          {showReadTime ? <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" /> : null}
+          {post.authors && `Fra ${post.authors.map((author) => author.fullName).join(', ')}`}
+          <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+        </div>
+        <div className="hidden sm:text-lg sm:block col-start-3 col-end-3 row-start-3 row-end-3 sm:ml-7">
+          {post.description && truncateText(toPlainText(post.description), MAX_DESCRIPTION_LENGTH)}{' '}
         </div>
       </div>
     </div>
