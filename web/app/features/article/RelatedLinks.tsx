3452331272bcb175e070c705652aa08f08974f83
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-22 20:49:37 +0100

    Forbedre relevante lenker litt

--- web/app/features/article/RelatedLinks.tsx
+++ web/app/features/article/RelatedLinks.tsx
@@ -1,19 +1,19 @@
-import { RelatedLink } from 'utils/sanity/types/sanity.types'
+import { POST_BY_SLUGResult } from 'utils/sanity/types/sanity.types'
 
 import { RelatedLinkElement } from '../../components/RelatedLink'
 
 type RelatedLinksProps = {
-  links: RelatedLink[]
+  links: NonNullable<NonNullable<POST_BY_SLUGResult>['relatedLinks']>
 }
 
 export const RelatedLinks = ({ links }: RelatedLinksProps) => {
   return (
     <div>
       <div className="mb-8 border-b border-bekk-night pb-1 text-body-mobile md:text-body-desktop" />
-      <h3>Her er noen relevante artikler som forfatteren anbefaler:</h3>
-      {links.map((link) => {
-        return <RelatedLinkElement key={link._key} link={link} />
-      })}
+      <h3>Her er noen relevante ressurser som forfatteren anbefaler:</h3>
+      {links.map((link) => (
+        <RelatedLinkElement key={link._key} link={link} />
+      ))}
     </div>
   )
 }
