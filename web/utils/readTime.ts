ec47254473053bd252a42cf72f53ff4e9730b36a
Vemund Thallaug Lund <vemund@thallaug.org>
2024-11-22 12:59:18 +0100

    fix: bug with reading time

--- web/utils/readTime.ts
+++ web/utils/readTime.ts
@@ -2,20 +2,20 @@ import { PortableText } from './sanity/types/sanity.types'
 import { toPlainText } from './sanity/utils'
 
 export const readingTime = (post: PortableText | undefined) => {
-  const averageWPM = 600
+  const averageWPM = 130
   const text = toPlainText(post)
+  const regex = /[A-Za-z']+/gm
+  const adjustedText = text
+    .replace(/<[^>]*>/gi, ' ')
+    .replace(/<\/[^>]*>/gi, ' ')
+    .replace(/&nbsp;|&#160;/gi, ' ')
+    .replace(/[\u200B-\u200F\u202A-\u202E\uFEFF\u2000-\u200F\u2028-\u202F]/g, '')
+    .replace(/\s+/gi, ' ')
+    .trim()
 
-  const adjustedText = text.replace(/(.)\1+/g, '$1')
+  const matches = [...adjustedText.matchAll(regex)]
 
-  const adjustedSentences = adjustedText.replace(/([.!?])\s*\1+/g, '$1')
-
-  const adjustedCharCount = adjustedSentences.length
-
-  const adjustedWords = adjustedSentences.trim().split(/\s+/)
-  const adjustedWordCount = adjustedWords.length
-  const averageWordLength = adjustedCharCount / adjustedWordCount
-
-  const adjustedTime = (adjustedCharCount / averageWPM) * (averageWordLength / 5)
+  const adjustedTime = matches.length / averageWPM
 
   const formattedAdjustedTime = adjustedTime > 1 ? Math.round(adjustedTime) + ' min' : 'Mindre enn 1 min'
 
