9d1e2939eb290841e8fb98b7b84ac65a4544ca56
Vemund Thallaug Lund <vemund@thallaug.org>
2024-11-22 08:23:52 +0100

    feat: add spinner when loading new page

---
+++ web/app/components/Spinner.tsx
@@ -0,0 +1,5 @@
+// components/Spinner.tsx
+
+const Spinner = () => <div className="spinner relative flex items-center justify-center"></div>
+
+export default Spinner
--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,6 +1,18 @@
 import type { LinksFunction, LoaderFunction } from '@remix-run/node'
-import { json, Links, Meta, Outlet, Scripts, ScrollRestoration, useMatches, useRouteError } from '@remix-run/react'
+import {
+  json,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useMatches,
+  useNavigation,
+  useRouteError,
+} from '@remix-run/react'
 import { generateSecurityHeaders } from 'utils/security'
+
+import { BekkChristmasHat } from './components/BekkChristmasHat'
 
 import { Header } from '~/features/navigation/Header'
 import { Page404 } from '~/routes/404'
@@ -36,6 +48,8 @@ export function ErrorBoundary() {
 export function Layout({ children }: { children: React.ReactNode }) {
   const matches = useMatches()
   const error = useRouteError()
+  const isLoading = useNavigation()
+
   type PotentialLanguageType = { language: string } | undefined
   const postData = matches.find((match) => (match.data as PotentialLanguageType)?.language)
     ?.data as PotentialLanguageType
@@ -52,15 +66,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <title>bekk.christmas</title>
       </head>
       <body className={`m-auto min-w-[375px] max-w-screen-2xl break-words bg-envelope-beige`}>
-        <div className={`${isInArticle && 'striped-frame md:my-8 md:mx-8 '}`}>
-          <header className={`${isInArticle && 'relative'}`}>
-            <Header isInArticle={isInArticle} />
-          </header>
-          <Scripts />
-          {children}
-          <ScrollRestoration />
-          <Scripts />
-        </div>
+        {isLoading.state === 'loading' ? (
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="loader">
+              <BekkChristmasHat />
+            </div>
+          </div>
+        ) : (
+          <div className={`${isInArticle && 'striped-frame md:my-8 md:mx-8 '}`}>
+            <header className={`${isInArticle && 'relative'}`}>
+              <Header isInArticle={isInArticle} />
+            </header>
+            <Scripts />
+            {children}
+            <ScrollRestoration />
+            <Scripts />
+          </div>
+        )}
       </body>
     </html>
   )
