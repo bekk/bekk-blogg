8c3a1e18d179eddc841216b98b89420827f97bef
Vemund Thallaug Lund <vemund@thallaug.org>
2024-11-22 15:49:54 +0100

    changes in readtime to improve readability

--- web/utils/readTime.ts
+++ web/utils/readTime.ts
@@ -2,18 +2,11 @@ import { PortableText } from './sanity/types/sanity.types'
 import { toPlainText } from './sanity/utils'
 
 export const readingTime = (post: PortableText | undefined) => {
-  const averageWPM = 130
+  const averageWPM = 150
   const text = toPlainText(post)
   const regex = /[A-Za-z']+/gm
-  const adjustedText = text
-    .replace(/<[^>]*>/gi, ' ')
-    .replace(/<\/[^>]*>/gi, ' ')
-    .replace(/&nbsp;|&#160;/gi, ' ')
-    .replace(/[\u200B-\u200F\u202A-\u202E\uFEFF\u2000-\u200F\u2028-\u202F]/g, '')
-    .replace(/\s+/gi, ' ')
-    .trim()
 
-  const matches = [...adjustedText.matchAll(regex)]
+  const matches = [...text.matchAll(regex)]
 
   const adjustedTime = matches.length / averageWPM
 
