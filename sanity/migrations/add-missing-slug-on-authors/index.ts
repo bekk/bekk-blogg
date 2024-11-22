7cb4749f42f451311dc77b8ae2e5d2f06b11f15d
Maja Testad <62019920+majatestad@users.noreply.github.com>
2024-11-22 13:39:51 +0100

    🛠️Add missing slug to authors (#115)
    # Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/S-rge-for-at-alle-forfattere-har-en-slug-1466bd30854180aa9ddad49e448c2f0a?pvs=4)
    
    🐛 Type oppgave: brukerhistorie
    
    🥅 Mål med PRen: sørge for at det ikke er brekte url'er fordi forfattere
    ikke har slug
    
    ## Løsning
    
    #️⃣ Punktliste av hva som er endret:
    
    - Laget migrering som generer slug på alle forfatterobjekter som mangler
    slug
    - Fjernet dobbel '💌' i breadcrumb
    
    ## 🧪 Testing
    
    Klikk deg inn på div forfatter og sjekk at det funker!
    
    ## Bilder
    
    **Før:**
    
    ![image](https://github.com/user-attachments/assets/ea64267b-3575-4833-8716-2c8bb54d3f80)
    **Etter:**
    
    
    ![image](https://github.com/user-attachments/assets/fe52fd43-bf0d-4e8c-9911-31b7eb55ddac)

---
+++ sanity/migrations/add-missing-slug-on-authors/index.ts
@@ -0,0 +1,21 @@
+import { at, defineMigration, patch, setIfMissing } from 'sanity/migrate'
+import slugify from 'slugify'
+
+/**
+ * this migration will set `slug` on all authors that are missing a slug
+ */
+export default defineMigration({
+  title: 'Add missing slug on authors',
+  documentTypes: ['author'],
+
+  async *migrate(documents) {
+    for await (const document of documents()) {
+      const authorName = document.fullName
+      if (authorName && !document.slug) {
+        console.log('Missing slug for author:', authorName)
+        const authorSlug = slugify(authorName.toString(), {lower: true, strict: true, trim: true})
+        yield patch(document._id, [at('slug.current', setIfMissing(authorSlug))])
+      }
+    }
+  },
+})
