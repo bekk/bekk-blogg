f091df100c907a315e7fb038979a806325f3b393
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-26 09:16:50 +0100

    Fiks brede lenker

--- web/app/features/post-preview/PostPreview.tsx
+++ web/app/features/post-preview/PostPreview.tsx
@@ -15,6 +15,7 @@ type PostPreviewType = {
   summary: string | null
   wordCount: number | null
   podcastLength: number | null
+  link?: string | null
 }
 
 type PostPreviewProps = PostPreviewType
@@ -27,42 +28,42 @@ export const PostPreview = ({
   summary,
   wordCount,
   podcastLength,
+  link,
 }: PostPreviewProps) => {
   const showReadTime = wordCount !== null && podcastLength === null
-  return (
-    <div className="striped-frame py-6 px-6 sm:p-7 max-w-4xl">
-      <div className="grid sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr] w-full">
-        <div className="col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
-          {title && <h2 className="font-delicious sm:mb-20">{title}</h2>}
-        </div>
-        <div className="col-start-2 row-start-1 row-end-4 hidden border-r border-bekk-night sm:block" />
-        <div className="flex justify-end ml-1 sm:mb-9 col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
-          <PostStamp image={coverImage} size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />
-        </div>
-        <div className="text-sm sm:text-lg col-start-1 col-end-1 row-start-3 row-end-3 sm:mr-7">
-          {tags && (
-            <>
-              {tags.join(', ')}
-              <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-            </>
-          )}
-          {showReadTime && (
-            <>
-              {podcastLength ? `${podcastLength} min` : wordCount ? readingTime(wordCount) : null}
-              <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-            </>
-          )}
-          {authors && `Fra ${authors.join(', ')}`}
-          <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
-        </div>
-        {summary && (
-          <div className="hidden sm:text-lg sm:block col-start-3 col-end-3 row-start-3 row-end-3 sm:ml-7">
-            <p className="line-clamp-6">{summary}</p>
-          </div>
-        )}
+  const content = (
+    <div className="grid sm:grid-cols-[1fr_1px_1fr] grid-cols-[30fr_1fr] w-full py-6 px-6 sm:p-7">
+      <div className="col-start-1 col-end-1 row-start-2 row-end-2 sm:mr-7">
+        {title && <h2 className="font-delicious sm:mb-20">{title}</h2>}
       </div>
+      <div className="col-start-2 row-start-1 row-end-4 hidden border-r border-bekk-night sm:block" />
+      <div className="flex justify-end ml-1 sm:mb-9 col-start-3 col-end-3 row-start-2 row-end-3 sm:ml-7">
+        <PostStamp image={coverImage} size={'h-16,05 w-12 sm:h-21,4 sm:w-16 sm:h-[107px] sm:w-[80px]'} />
+      </div>
+      <div className="text-sm sm:text-lg col-start-1 col-end-1 row-start-3 row-end-3 sm:mr-7">
+        {tags && (
+          <>
+            {tags.join(', ')}
+            <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+          </>
+        )}
+        {showReadTime && (
+          <>
+            {podcastLength ? `${podcastLength} min` : wordCount ? readingTime(wordCount) : null}
+            <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+          </>
+        )}
+        {authors && `Fra ${authors.join(', ')}`}
+        <div className="sm:mb-7 border-b border-bekk-night pb-1 mb-3" />
+      </div>
+      {summary && (
+        <div className="hidden sm:text-lg sm:block col-start-3 col-end-3 row-start-3 row-end-3 sm:ml-7">
+          <p className="line-clamp-6">{summary}</p>
+        </div>
+      )}
     </div>
   )
+  return <div className="striped-frame max-w-4xl">{link ? <Link to={link}>{content}</Link> : content}</div>
 }
 
 // TODO: Refactor out to somewhere else
@@ -81,9 +82,9 @@ export const PostPreviewList = ({ posts }: PostPreviewListProps) => {
   return (
     <div className="flex flex-col max-sm:w-full gap-8 md:gap-12">
       {posts.map((post) => (
-        <Link key={post._id} to={postUrl(post)} className="mx-4 flex justify-center">
-          <PostPreview {...post} />
-        </Link>
+        <div key={post._id} className="flex justify-center">
+          <PostPreview {...post} link={postUrl(post)} />
+        </div>
       ))}
     </div>
   )
