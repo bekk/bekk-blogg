4d293e67ed509e507bf346ddd176e851914948a1
Vemund Thallaug Lund <80754916+vemundtl@users.noreply.github.com>
2024-11-26 09:07:13 +0100

    Fix/border jump on other embeddings (#140)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Fikse-s-nn-at-andre-embeddings-ikke-gj-r-at-border-hopper-1456bd30854180dbbd4dc01dfd4d1573?pvs=4)
    
    🐛 FIKS
    
    🥅 Mål med PRen: Håndtere embeddings slik at border ikke hopper
    
    ## Løsning
    
    - Sette aspect ratio på youtube, vimeo og podcastblock

--- web/app/portable-text/VimeoBlock.tsx
+++ web/app/portable-text/VimeoBlock.tsx
@@ -1,20 +1,16 @@
-import useMediaQuery from '~/hooks/useMediaQuery'
-
 type VimeoBlockProps = { src: string; title: string }
 
 export default function VimeoBlock({ video }: { video: VimeoBlockProps }) {
-  const isSm = useMediaQuery('(min-width: 640px)')
-  const height = isSm ? '500px' : '300px'
-
   return (
-    <iframe
-      src={video.src}
-      title={video.title}
-      width="100%"
-      height={height}
-      allow="autoplay; fullscreen; picture-in-picture"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    />
+    <div className="aspect-video w-full">
+      <iframe
+        src={video.src}
+        title={video.title}
+        className="w-full h-full"
+        allow="autoplay; fullscreen; picture-in-picture"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+    </div>
   )
 }
