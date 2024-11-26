b120c4d8085616311a1fba2379ebd7c0b4eeed01
Anna Li <halloannali@gmail.com>
2024-11-26 13:23:17 +0100

    white bg for se tidligere julekalendere

--- web/app/components/LinkToArchive.tsx
+++ web/app/components/LinkToArchive.tsx
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react'
 export const LinkToArchive = () => {
   return (
     <div className="flex justify-center mt-16">
-      <Link to={'/archive'} className="text-xl text-black hover:text-reindeer-brown underline">
+      <Link to={'/archive'} className="text-xl text-black hover:text-reindeer-brown underline bg-white p-2">
         Se tidligere julekalendere
       </Link>
     </div>
