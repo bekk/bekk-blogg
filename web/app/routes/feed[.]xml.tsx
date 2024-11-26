7494c940dae0632e48ad59cf27be0f239e161748
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-26 21:29:09 +0100

    Legg til RSS feed

--- web/app/routes/feed[.]xml.tsx
+++ web/app/routes/feed[.]xml.tsx
@@ -9,7 +9,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const baseUrl = new URL(request.url).origin
 
   const rssString = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>Bekk Christmas</title>
     <description>Bekk Christmas er en julekalender med innhold om teknologi, design, strategi og produktutvikling fra Bekk</description>
