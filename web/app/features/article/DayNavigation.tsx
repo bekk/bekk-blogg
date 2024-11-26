bf67dd8a6bc6af0502bc7d274ff4d65080d59d37
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-26 20:40:03 +0100

    Fiks post-lenker i day navigation komponenten

--- web/app/features/article/DayNavigation.tsx
+++ web/app/features/article/DayNavigation.tsx
@@ -8,13 +8,21 @@ export const DayNavigation = ({ day, year }: DayNavigationProps) => {
   return (
     <div className="flex justify-center gap-4 my-8">
       {day > 1 && (
-        <Link to={`/${year}/${day - 1}`} prefetch="intent" className="group text-bekk-night hover:text-reindeer-brown">
+        <Link
+          to={`/post/${year}/${day - 1}`}
+          prefetch="intent"
+          className="group text-bekk-night hover:text-reindeer-brown"
+        >
           <span className="group-hover:-translate-x-1 group-hover:transition-all inline-block ">&larr;</span> {day - 1}.
           desember
         </Link>
       )}
       {day < 24 && (
-        <Link to={`/${year}/${day + 1}`} prefetch="intent" className="group text-bekk-night hover:text-reindeer-brown">
+        <Link
+          to={`/post/${year}/${day + 1}`}
+          prefetch="intent"
+          className="group text-bekk-night hover:text-reindeer-brown"
+        >
           {day + 1}. desember{' '}
           <span className="group-hover:translate-x-1 group-hover:transition-all inline-block">&rarr;</span>
         </Link>
