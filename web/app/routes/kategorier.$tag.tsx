bb68de26a6f693fc30ee40068a9d0290002b872e
Natasha Tikhonova <nataliyatikhonova01@gmail.com>
2024-11-26 13:09:56 +0100

    rename tags to kategorier

--- web/app/routes/kategorier.$tag.tsx
+++ web/app/routes/kategorier.$tag.tsx
@@ -87,7 +87,7 @@ export default function Tags() {
             )}
           </div>
           <PostPreviewList posts={posts} />
-          <Pagination {...pagination} baseUrl={`/tags/${tag.slug}`} />
+          <Pagination {...pagination} baseUrl={`/kategorier/${tag.slug}`} />
         </div>
       )}
     </>
