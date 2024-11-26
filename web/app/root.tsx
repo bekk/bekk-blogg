80ca04227bbe49a326d95978e3623aeec80e3b19
Anna Li <halloannali@gmail.com>
2024-11-26 13:14:39 +0100

    added bg on teaser and article page

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react'
 import type { LinksFunction, LoaderFunction } from '@remix-run/node'
 import {
   json,
@@ -7,20 +8,19 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useLocation,
   useMatches,
   useRouteError,
 } from '@remix-run/react'
 import { VisualEditing } from '@sanity/visual-editing/remix'
-import { lazy, Suspense } from 'react'
 import { loadQueryOptions } from 'utils/sanity/loadQueryOptions.server'
 import { generateSecurityHeaders } from 'utils/security'
 
-import { ArticleBackgroundSVG } from './features/article/ArticleBackgroundSVG'
+import { JumpToContent } from './features/jump-to-content/JumpToContent'
 
 import { Header } from '~/features/navigation/Header'
 import { Page404 } from '~/routes/404'
 import styles from '~/styles/main.css?url'
-import { JumpToContent } from './features/jump-to-content/JumpToContent'
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
@@ -49,12 +49,20 @@ export function ErrorBoundary() {
 export function Layout({ children }: { children: React.ReactNode }) {
   const matches = useMatches()
   const error = useRouteError()
+  const location = useLocation()
 
   type PotentialLanguageType = { language: string } | undefined
   const postData = matches.find((match) => (match.data as PotentialLanguageType)?.language)
     ?.data as PotentialLanguageType
 
   const isInArticle = matches.some((match) => match.params.slug) && !error
+
+  const bodyClass =
+    location.pathname === '/'
+      ? `bg-[url('/images/bg_teaser.svg')] bg-no-repeat bg-cover bg-center`
+      : isInArticle
+        ? `bg-[url('/images/bg_wooden.svg')] bg-no-repeat bg-cover bg-center`
+        : 'bg-envelope-beige'
 
   return (
     <html lang={postData?.language ?? 'nb-NO'}>
@@ -66,13 +74,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <link rel="alternate" type="application/rss+xml" title="Bekk Christmas RSS Feed" href="/feed.xml" />
         <script defer data-domain="bekk.christmas" src="https://plausible.io/js/plausible.js" />
       </head>
-      <body className={`m-auto min-w-[375px] max-w-screen-2xl break-words bg-envelope-beige`}>
+      <body className={`m-auto min-w-[375px] max-w-screen-2xl break-words ${bodyClass}`}>
         <JumpToContent />
-        {isInArticle && (
-          <div className="fixed inset-0 -z-10">
-            <ArticleBackgroundSVG />
-          </div>
-        )}
         <div className={`${isInArticle && 'striped-frame md:my-8 md:mx-8 '}`}>
           <header className={`${isInArticle && 'relative'}`}>
             <Header isInArticle={isInArticle} />
