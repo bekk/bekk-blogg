9b7e4410f45aa200f627c11b4892a52c66ac59d8
Vemund Thallaug Lund <80754916+vemundtl@users.noreply.github.com>
2024-11-26 11:52:37 +0100

    styling: general styling article page (#136)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Fikse-bredde-p-artikkelsiden-1496bd30854180018bf2ff6682ccdbae?pvs=4)
    
    💄 STYLING
    
    🥅 Mål med PRen: Fikse artikkelsiden til akseptabel standard
    
    ## Løsning
    
    #️⃣ Punktliste av hva som er endret:
    
    - Fiksa slik at bilder osv ikke går utenfor container 
    - Fiksa slik at venstre kolonne (meta) alltid har en viss plass slik at
    det ikke blir en tynn stripe
    - Aligne breadcrumbs og meta 
    - Breakpoints 
    
    **NB! Dette går nok helt sikkert an å fikse på en måte slik at kolonnene
    vokser basert på skjermstørrelsen istedenfor å endre på breakpoints, men
    for nå så får det være og så kan noen som er litt flinkere på styling
    fikse senere**

--- web/app/features/article/Article.tsx
+++ web/app/features/article/Article.tsx
@@ -1,7 +1,7 @@
+import { Fragment, useRef, useState } from 'react'
 import { PortableText } from '@portabletext/react'
 import { Link } from '@remix-run/react'
 import { Loader, Pause, Play } from 'lucide-react'
-import { Fragment, useRef, useState } from 'react'
 import { trackEvent } from 'utils/analytics'
 import { formatDate } from 'utils/date'
 import { readingTime } from 'utils/readTime'
@@ -76,9 +76,9 @@ export const Article = ({ post }: ArticleProps) => {
     return null
   }
   return (
-    <div className="px-6 sm:grid-cols-[1fr_2fr] md:grid md:grid-rows-[auto_auto] md:gap-x-12 xl:gap-x-24 md:gap-y-6 md:pl-20 pb-8 md:pb-16">
-      <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 mb-8">
-        <h1 className="font-delicious">{post.title}</h1>
+    <div className="px-6 sm:grid-cols-[1fr_2fr] md:grid md:grid-rows-[auto_auto] md:gap-x-12 xl:gap-x-24 md:gap-y-6 md:pl-10 xl:pl-20 pb-8 md:pb-16">
+      <div className="meta col-start-1 col-end-1 row-start-2 row-end-2 mb-8 md:min-w-[230px] lg:min-w-[240px] 2lg:min-w-[250px]">
+        <h1 className="sm:mb-4 text-3xl sm:text-4xl font-delicious">{post.title}</h1>
         {post.tags && (
           <div>
             {post.tags.map((tag, index) => (
@@ -120,9 +120,9 @@ export const Article = ({ post }: ArticleProps) => {
           <AudioPlayer src={`/api/tts?id=${post._id}`} slug={post.slug?.current ?? 'unknown'} />
         )}
       </div>
-      <div className="col-start-2 col-end-2 row-start-2 row-end-2 max-md:max-w-screen-xl max-lg:max-w-lg max-xl:max-w-xl">
+      <div className="flex flex-col col-start-2 col-end-2 row-start-2 row-end-2 max-md:max-w-screen-xl max-lg:max-w-[475px] max-2lg:max-w-[550px] 2lg:max-w-[675px] xl:pr-10 xl:max-w-3xl 2xl:max-w-4xl">
         {post?.description ? (
-          <div className="text-2xl">
+          <div className="text-xl">
             <PortableText value={post.description} components={components} />
           </div>
         ) : null}
