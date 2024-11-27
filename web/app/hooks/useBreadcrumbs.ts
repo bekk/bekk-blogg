2505ee077a18a1bbbe6f84428c69103b12d8c6cb
Vemund Thallaug Lund <80754916+vemundtl@users.noreply.github.com>
2024-11-27 09:54:53 +0100

    add breadcrumbs to category and author page (#150)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Oppdatere-breadcrumbs-p-forfatter-og-kategorier-1496bd3085418045a7d3e1f45cb0236b?pvs=4)
    
    🛠️ FIKS
    
    🥅 Mål med PRen: Legge til breadcrumbs for kategori- og forfattersiden
    
    ## Løsning
    
    - Gjøre endringer til useBreadCrumbs-funksjonen så den håndterer
    kategori og forfattersiden
    
    ## Bilder
    
    ![image](https://github.com/user-attachments/assets/f2c44090-3029-421a-80a0-0098b9f999c9)
    
    ![image](https://github.com/user-attachments/assets/15109205-ff29-4139-983d-c79747bd08ce)
    
    
    **NB! Per nå vil man routes tilbake til 2024 uansett om man gikk inn på
    kategorisiden fra en artikkel fra 2020 f.eks.**

--- web/app/hooks/useBreadcrumbs.ts
+++ web/app/hooks/useBreadcrumbs.ts
@@ -13,32 +13,61 @@ export function useBreadcrumbs(): Breadcrumb[] {
   const breadcrumbs: Breadcrumb[] = []
   let accumulatedPath = ''
 
-  Object.keys(currRoute.params).map((key) => {
-    accumulatedPath += `/${currRoute.params[key]}`
+  const addBreadcrumb = (path: string, title: string) => {
+    accumulatedPath += path
+    breadcrumbs.push({ href: accumulatedPath, title })
+  }
 
-    let title = ''
+  if (currRoute.pathname.includes('/post/')) {
+    accumulatedPath += '/post'
+    Object.keys(currRoute.params).map((key) => {
+      accumulatedPath += `/${currRoute.params[key]}`
 
-    if (key === 'year') {
-      if (currRoute.params.year && new Date(new Date().setFullYear(parseInt(currRoute.params.year))) < new Date()) {
-        title = `📯 ${currRoute.params.year}`
-      } else {
-        title = '📯 Postkontoret'
+      let title = ''
+      if (key === 'year') {
+        if (currRoute.params.year && new Date(new Date().setFullYear(parseInt(currRoute.params.year))) < new Date()) {
+          title = `📯 ${currRoute.params.year}`
+        } else {
+          title = '📯 Postkontoret'
+        }
+      } else if (key === 'date') {
+        let date = currRoute.params.date
+        if (date && parseInt(date) < 10) {
+          date = date.replace('0', '')
+        }
+        title = `📬 ${date}. des`
+      } else if (key === 'slug' && currRoute.data) {
+        const postTitle = (currRoute.data as { initial: { data: Post } }).initial.data?.title
+        title = `💌 ${postTitle ?? ' Innlegg'}`
       }
-    } else if (key === 'date') {
-      let date = currRoute.params.date
-      if (date && parseInt(date) < 10) {
-        date = date.replace('0', '')
-      }
-      title = `📬 ${date}. des`
-    } else if (key === 'slug' && currRoute.data) {
-      const postTitle = (currRoute.data as { initial: { data: Post } }).initial.data?.title
-      title = `💌 ${postTitle ?? ' Innlegg'}`
-    }
 
-    breadcrumbs.push({
-      href: accumulatedPath,
-      title: title,
+      breadcrumbs.push({
+        href: accumulatedPath,
+        title: title,
+      })
     })
-  })
+  } else if (currRoute.pathname.includes('/kategori')) {
+    breadcrumbs.unshift({
+      href: '/post/2024',
+      title: '📯 Postkontoret',
+    })
+    addBreadcrumb('/kategori', '📚 Kategorier')
+    Object.entries(currRoute.params).forEach(([key, value]) => {
+      if (key === 'tag' && value && currRoute.data) {
+        const tagName = (currRoute.data as { tag: { name: string } }).tag.name
+        addBreadcrumb(`/${value}`, `💌 ${tagName.charAt(0).toUpperCase() + tagName.slice(1)}`)
+      }
+    })
+  } else if (currRoute.pathname.includes('/forfatter')) {
+    addBreadcrumb('/post/2024', '📯 Postkontoret')
+    accumulatedPath += '/forfatter'
+    addBreadcrumb(
+      accumulatedPath,
+      `✍️ ${currRoute.params.name
+        ?.split('-')
+        .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+        .join(' ')}`
+    )
+  }
   return breadcrumbs
 }
