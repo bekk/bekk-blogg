4257df560f9049655de21e1bb0da9fb156f10f5c
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-29 14:02:11 +0100

    Legg til samlinger i studioet

--- sanity/structure/index.ts
+++ sanity/structure/index.ts
@@ -7,4 +7,5 @@ export const structure: StructureResolver = (S) =>
       S.documentTypeListItem('post').title('Alle innlegg'),
       S.documentTypeListItem('author').title('Forfattere'),
       S.documentTypeListItem('tag').title('Kategorier'),
+      S.documentTypeListItem('collection').title('Samlinger'),
     ])
