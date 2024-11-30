a9cde83aecce9380898b01124ab767c03bda0ec3
Vemund Thallaug Lund <80754916+vemundtl@users.noreply.github.com>
2024-11-30 23:37:22 +0100

    new spinner (#199)
    ## Beskrivelse
    
    💳 Lenke til
    [Notionkort](https://www.notion.so/bekks/Lage-spinner-n-r-man-g-r-inn-p-en-forfatter-eller-en-kategori-fra-en-artikkel-14d6bd30854180febd18fef8116460ee?pvs=4)
    
    💄  Styling
    
    - Lagd ny spinner på kategori 
    - Lagt til spinner på artikkelsiden slik at man får litt feedback når
    man har trykt
    
    
    
    ![image](https://github.com/user-attachments/assets/1bbb5af9-5b72-4fc8-aa36-4bfa35c78c1d)
    
    ![image](https://github.com/user-attachments/assets/f850cb58-13d0-43e8-8eb5-72852dbe6c63)

--- web/app/components/Spinner.tsx
+++ web/app/components/Spinner.tsx
@@ -1,7 +1,5 @@
-import { BekkChristmasHat } from './BekkChristmasHat'
-
 export const Spinner = () => (
-  <div className="loader" aria-label="Laster inn kategorier" aria-live="polite">
-    <BekkChristmasHat />
+  <div className="flex flex-col justify-center items-center">
+    <div className="spinner w-12 h-12 sm:w-24 sm:h-24" aria-label="Laster inn kategorier" aria-live="polite" />
   </div>
 )
