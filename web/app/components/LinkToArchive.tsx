f42689d8daabbfe2b0ebedf1b2c551394e26886c
majatestad <maja_ying@hotmail.com>
2024-12-09 12:10:41 +0100

    update route redirect

--- web/app/components/LinkToArchive.tsx
+++ web/app/components/LinkToArchive.tsx
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react'
 export const LinkToArchive = () => {
   return (
     <div className="flex justify-center mt-16">
-      <Link to="/ArchiveRoute" className="gift-tag text-xl text-black hover:text-reindeer-brown underline">
+      <Link to="/arkiv" className="gift-tag text-xl text-black hover:text-reindeer-brown underline">
         Se tidligere julekalendere
       </Link>
     </div>
