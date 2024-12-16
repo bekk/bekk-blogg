6d73f18299894c99dcfecbc5b253f434e6e79daf
majatestad <maja_ying@hotmail.com>
2024-12-16 15:54:49 +0100

    remove animation on door sign

--- web/app/components/DoorSign.tsx
+++ web/app/components/DoorSign.tsx
@@ -10,14 +10,6 @@ export const DoorSign = ({ children }: Props) => {
       <div className="h-[61px] w-[283px] 2lg:h-[74px] 2lg:w-[340px] flex justify-center items-center bg-[url('/images/doorsign.svg')] focus-visible:bg-[url('/images/doorsign_focus.svg')] focus-visible:text-light-brown active:text-dark-brown bg-contain bg-no-repeat text-base 2lg:text-lg text-black focus-visible:no-underline">
         {children}
       </div>
-      <motion.div
-        className="absolute inset-0 bg-gradient-to-r from-transparent via-white to-transparent opacity-0 pointer-events-none"
-        variants={{
-          rest: { opacity: 0, x: '-10%' },
-          hover: { opacity: 0.5, x: '10%', transition: { duration: 0.3 } },
-        }}
-        style={{ mixBlendMode: 'overlay' }}
-      />
     </motion.div>
   )
 }
