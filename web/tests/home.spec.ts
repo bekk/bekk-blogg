fc22e2383893dd86f218498107d2ef3cce1d18ef
Jakob Helgøy <jakobnhelgoy@gmail.com>
2025-10-15 14:50:14 +0200

    Teaserpage should link to old calendar and comment out tests which are based on the 2024 calendar

--- web/tests/home.spec.ts
+++ web/tests/home.spec.ts
@@ -1,28 +1,26 @@
-import { expect, test } from '@playwright/test'
+// test('home page redirects to year after December 1st', async ({ page }) => {
+//   await page.goto('/')
 
-test('home page redirects to year after December 1st', async ({ page }) => {
-  await page.goto('/')
+//   // Should redirect to /post/YYYY
+//   await expect(page).toHaveURL(new RegExp(`/post/\\d{4}$`))
+// })
 
-  // Should redirect to /post/YYYY
-  await expect(page).toHaveURL(new RegExp(`/post/\\d{4}$`))
-})
+// test('navigating all the way to a post in 2024 works as expected', async ({ page }) => {
+//   await page.goto('/post/2024')
 
-test('navigating all the way to a post in 2024 works as expected', async ({ page }) => {
-  await page.goto('/post/2024')
+//   await expect(page).toHaveURL('/post/2024')
 
-  await expect(page).toHaveURL('/post/2024')
+//   const dayOne = page.getByLabel('Luke 1', { exact: true })
+//   await expect(dayOne).toBeVisible()
+//   await dayOne.click()
 
-  const dayOne = page.getByLabel('Luke 1', { exact: true })
-  await expect(dayOne).toBeVisible()
-  await dayOne.click()
+//   await expect(page).toHaveURL('/post/2024/01')
 
-  await expect(page).toHaveURL('/post/2024/01')
+//   await expect(page.getByRole('heading', { name: '1. desember' })).toBeVisible()
+//   await expect(page.getByText('Velkommen til en helt ny julefeiring')).toBeVisible()
+//   await page.getByText('Velkommen til en helt ny julefeiring').click()
 
-  await expect(page.getByRole('heading', { name: '1. desember' })).toBeVisible()
-  await expect(page.getByText('Velkommen til en helt ny julefeiring')).toBeVisible()
-  await page.getByText('Velkommen til en helt ny julefeiring').click()
-
-  await expect(page).toHaveURL('/post/2024/01/velkommen-til-en-helt-ny-julefeiring', { timeout: 60_000 })
-  await expect(page).toHaveTitle('Velkommen til en helt ny julefeiring | Bekk Christmas')
-  await expect(page.getByRole('heading', { name: 'Velkommen til en helt ny julefeiring', level: 1 })).toBeVisible()
-})
+//   await expect(page).toHaveURL('/post/2024/01/velkommen-til-en-helt-ny-julefeiring', { timeout: 60_000 })
+//   await expect(page).toHaveTitle('Velkommen til en helt ny julefeiring | Bekk Christmas')
+//   await expect(page.getByRole('heading', { name: 'Velkommen til en helt ny julefeiring', level: 1 })).toBeVisible()
+// })
